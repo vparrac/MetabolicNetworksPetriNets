29dbfa385b9eea706cedb418b32bcb99c15ae8e7
Valerie Parra Cortés <v.parrac@uniandes.edu.co>
2020-09-01 07:36:48 -0500

    Processing listo para moverse. Falta corregir para que solo corra el offset

--- src/metapenta/visualization/petrinet/MySketch.java
+++ src/metapenta/visualization/petrinet/MySketch.java
@@ -1,30 +1,27 @@
 package metapenta.visualization.petrinet;
 import processing.core.PApplet;
 public class MySketch extends PApplet {
-
 	private boolean locked=false;;
+	// Colors
 	private final static RGBTuple BLUE = new RGBTuple(3, 152, 158); 
 	private static final RGBTuple BLUE_KING = new RGBTuple(0, 74, 173);
 	private static final RGBTuple ORANGE = new RGBTuple(255, 145, 77);
-	private float bx;
-	private float by;
-	private int bs = 40;
-	private int bz = 30;
-	private boolean bover = false;
-	private float bdifx = 0; 
-	private float bdify = 0; 
+	
+	private final static int BS = 50;	
+	private boolean bover = false;	
 	private float newx, newy;
 	int whichImage;
-	private float[] positions_transitions= new float [3*2] ;
-	private float[] positions_places = new float[0];
-	private boolean in_transition=false;
-	private int radious=20;
-
-
+	private float[] positions_transitions = {200,20,250,20} ;
+	private float[] positions_places = {100,20,250,30} ;
+	private float xOffset = 0; 
+	private float yOffset = 0; 
+	
+	
+	private boolean isTransition=false;
+	private int radious=50;
 	public static void main(String[] args) {
 		PApplet.main(MySketch.class.getName());
 	}
-
 	public void settings() {
 		size(640, 640);
 		smooth();
@@ -33,30 +30,28 @@ public class MySketch extends PApplet {
 		}
 	}	 
 
-
 	public void setup() {
-		bx = width/2;
-		by = height/2;
-		for (int j=0; j < 3*2; j+=2) {
-			positions_transitions[j]= random(width-50);
-			positions_transitions[j+1]= random(height-50);
-//			positions_places[j]= random(width-50);
-//			positions_places[j+1]= random(height-50);
-			
-		}	  
-		fill(153);
+
 	}
 
-	public void draw() 
-	{ 
+	public void draw() {		
 		background(255, 255, 255);	
-		for (int j=0; j < 3; j++) {
+		for (int j=0; j < positions_transitions.length/2; j++) {
 			if (bover && whichImage==j) 
 				stroke(255);  // white
 			else
 				noStroke(); 
-			fill(255,145,77);
-			rect ( positions_transitions[j*2], positions_transitions[j*2+1], 50, 50) ;
+			fill(BLUE_KING.r,BLUE_KING.g,BLUE_KING.b);
+			rect ( positions_transitions[j*2], positions_transitions[j*2+1], BS, BS) ;
+		}
+
+		for (int j=0; j < positions_places.length/2; j++) {
+			if (bover && whichImage==j) 
+				stroke(255);  
+			else
+				noStroke(); 
+			fill(ORANGE.r,ORANGE.g,ORANGE.b);
+			ellipse ( positions_places[j*2], positions_places[j*2+1], radious, radious) ;
 		}
 	}
 
@@ -66,8 +61,9 @@ public class MySketch extends PApplet {
 			locked = true;
 		} 
 		else {
-			locked = false;
+			locked = false;			
 		}
+
 	}
 
 	public void mouseReleased() {
@@ -77,39 +73,56 @@ public class MySketch extends PApplet {
 
 	public void mouseDragged() {
 		if (locked) {
+//			float[] node = (isTransition)? positions_transitions:positions_places
 			newx = mouseX; 
 			newy = mouseY;
 		}
 
-		positions_transitions [whichImage*2] = newx;
-		positions_transitions [(whichImage*2)+1] = newy;
+		moveNodes();
+	}
+
+	void moveNodes() {
+		if(isTransition) {
+			positions_transitions [whichImage*2] = newx;
+			positions_transitions [(whichImage*2)+1] = newy;
+		}
+		else if(!isTransition) {			
+			positions_places [whichImage*2] = newx;
+			positions_places [(whichImage*2)+1] = newy;
+		}
 	}
 
 	void checkOver() {
-		for (int i=0; i < 3; i++) {
-			if (mouseX > positions_transitions[i*2]-bs && mouseX < positions_transitions[i*2]+bs && 
-					mouseY > positions_transitions[i*2+1]-bs && mouseY < positions_transitions[i*2+1]+bs){
-				println ("mouseover image: "+i);
-				whichImage=i;
+		boolean found=false;
+		
+		for (int i = 0; i <positions_places.length/2 ; i++) {
+			System.out.println("Verificando places");
+			if (Math.sqrt(Math.pow((mouseY-positions_places[i*2+1]),2)+Math.pow((mouseX-positions_places[i*2]),2))<= radious) {				
+				System.out.println("Es place");
+				whichImage=i;	
 				bover = true;  
-				in_transition=true;
+				isTransition=false;
+				found=true;
 				break; 
-			} 
-			else{
-				in_transition=false;
-				bover = false;
-			}
-		}
-		for (int i = 0; i < positions_places.length/2; i++) {
-			if (Math.sqrt(Math.pow((mouseX-positions_places[i*2]),2)+Math.pow((mouseY-positions_places[i*2+1]),2))<= radious) {	
-				bover = true;  
-				whichImage=i;
-				in_transition=false;
 			} else {
+				isTransition=false;
 				bover = false;
 			}
-
-
 		}
+		
+		if(!found) {
+			for (int i=0; i < positions_transitions.length/2; i++) {		
+				System.out.println("Verificando transitions");
+				if (mouseX > positions_transitions[i*2]-BS && mouseX < positions_transitions[i*2]+BS && 
+						mouseY > positions_transitions[i*2+1]-BS && mouseY < positions_transitions[i*2+1]+BS){
+					whichImage=i;
+					bover = true;  
+					isTransition=true;				
+					break; 
+				} 
+			}
+		}		
+		System.out.println("Es transición "+isTransition);
+		System.out.println("bover "+bover);
 	}
 }
