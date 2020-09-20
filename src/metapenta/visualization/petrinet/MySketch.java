65d3e926396fb85e8c67b5d4da6ed277b1ba9e64
Valerie Parra Cortés <v.parrac@uniandes.edu.co>
2020-09-20 15:30:45 -0500

    Flechas cuadrados

--- src/metapenta/visualization/petrinet/MySketch.java
+++ src/metapenta/visualization/petrinet/MySketch.java
@@ -61,13 +61,13 @@ public class MySketch extends PApplet {
 	 * 2 means edge Place->Transition
 	 * 3 means edge Place->Transition and Transition->Place
 	 */
-	
+
 	private byte[][] adjacencyMatrix;
 	/**
 	 * The main method of the Sketch class
 	 * @param args 
 	 */
-	
+
 	public static void main(String[] args) {
 		PApplet.main(MySketch.class.getName());
 	}
@@ -78,7 +78,7 @@ public class MySketch extends PApplet {
 			frame.setResizable(true);
 		}
 	}	 
-	
+
 	/**
 	 * This method print an arrow
 	 * @param x1 the first coordinate of the arrow's start
@@ -104,11 +104,11 @@ public class MySketch extends PApplet {
 		positionsPlaces.add(new Place(200, 300, BS, BS, "2", ORANGE, WHITE));	
 		adjacencyMatrix = new byte[positionTransitions.size()][positionsPlaces.size()];
 		adjacencyMatrix[0][0]=1;
-		adjacencyMatrix[0][1]=1;
+		adjacencyMatrix[0][1]=2;
 		adjacencyMatrix[1][0]=1;
 		adjacencyMatrix[1][1]=1;
 	}
-	
+
 	public void draw() {		
 		background(255, 255, 255);
 		fill(BLUE_KING.r,BLUE_KING.g,BLUE_KING.b);	
@@ -122,7 +122,10 @@ public class MySketch extends PApplet {
 					arrow(x1, y1, coordinates[0],coordinates[1]);					
 				}
 				else if(adjacencyMatrix[i][j]==2) {
-					line(positionsPlaces.get(j).getPx(), positionsPlaces.get(j).getPy(),positionTransitions.get(i).getPx(), positionTransitions.get(i).getPy());
+					float[] intersectionPoints = intersectionPointRectLine(positionsPlaces.get(j).getPx(), positionsPlaces.get(j).getPy() ,positionTransitions.get(i).getPx()+BS/2, positionTransitions.get(i).getPy()+BS/2);
+					System.out.println(Arrays.toString(intersectionPoints));
+					arrow(positionsPlaces.get(j).getPx(), positionsPlaces.get(j).getPy(), intersectionPoints[0],intersectionPoints[1]);
+					line(positionsPlaces.get(j).getPx(), positionsPlaces.get(j).getPy(),positionTransitions.get(i).getPx()+BS/2, positionTransitions.get(i).getPy()+BS/2);
 				}
 				else if(adjacencyMatrix[i][j]==3) {
 					line(positionsPlaces.get(j).getPx(), positionsPlaces.get(j).getPy(),positionTransitions.get(i).getPx(), positionTransitions.get(i).getPy());
@@ -140,10 +143,10 @@ public class MySketch extends PApplet {
 			fill(ORANGE.r,ORANGE.g,ORANGE.b);
 			ellipse( positionsPlaces.get(j).getPx(), positionsPlaces.get(j).getPy(), radius, radius);			
 		}
-		
-		
+
+
 	}
-	
+
 	public void mousePressed() {		
 		checkOver();
 		if (bover) { 
@@ -198,27 +201,61 @@ public class MySketch extends PApplet {
 			}
 		}		
 	}
-	
-	
-	
+
+
+
 	private float[] intersectionPointCircleLine(double x1, double y1, double cx2, double cy2) {
-		double dx = cx2-x1;
-		double dy = cy2-y1;
+		double dx = cx2-x1, dy = cy2-y1;
 		double norm = norm(dx, dy);
 		double factor = (norm-radius/2)/norm;
-		double ndx = dx*factor+x1;
-		double ndy = dy*factor+y1;
+		double ndx = dx*factor+x1, ndy = dy*factor+y1;
 		float[] coordinates = new float[2];
 		coordinates[0] = (float) ndx;
 		coordinates[1] = (float) ndy;
 		return coordinates;		
 	}
-	
-	
+
+
+	private float[] intersectionPointRectLine(double x1, double y1, double cx2, double cy2) {
+		double dx = cx2-x1, dy = cy2-y1;
+		float[] points = new float[2];
+		if( (dx >= 0 && Math.abs(dy) <= Math.abs(dx)) || (dx <= 0 && Math.abs(dy) <= Math.abs(dx)) ) { // Left
+			double xl = (dx <= 0) ? cx2 + BS/2: cx2 - BS/2;
+			double[] lineEquation = lineEquation(x1, y1, cx2, cy2);
+			double yl = lineEquation[0]*xl + lineEquation[1];
+			points[0] = (float) xl;
+			points[1] = (float) yl;
+		}		
+		else if( (dy >= 0 && Math.abs(dy) >= Math.abs(dx)) || (dy <= 0 && Math.abs(dy) >= Math.abs(dx))) {			
+			double yl = (float) ((dy>=0)? cy2 - BS/2: cy2 + BS/2);
+			if(cx2==x1) {
+				points[0] = (float) cx2;
+				points[1] = (float) yl;
+			}
+			else {
+				double[] lineEquation = lineEquation(x1, y1, cx2, cy2);
+				points[0] = (float) ((yl-lineEquation[1])/lineEquation[0]);
+				points[1] = (float) yl;
+			}
+		}
+		return points;
+	}
+
+
+
 	private double norm(double dx, double dy) {
 		return Math.sqrt(Math.pow(dx, 2)+Math.pow(dy, 2));		
 	}
 	
-	
-	
+	private double[] lineEquation (double x1, double y1, double x2, double y2) {
+		double m = (y2-y1)/(x2-x1);
+		double b = y1-((y2-y1)/(x2-x1))*x1;
+		double[] lineEquation = new double[2];
+		lineEquation[0] = m;
+		lineEquation[1] = b;		
+		return lineEquation;
+	}
+
+
+
 }
