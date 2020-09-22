2cbe3691c4d08f031411abff44b0af685aed9329
Valerie Parra Cortés <v.parrac@uniandes.edu.co>
2020-09-22 11:25:49 -0500

    Acabando de imprimir connected components

--- src/metapenta/commands/ConnectedComponents.java
+++ src/metapenta/commands/ConnectedComponents.java
@@ -1,4 +1,9 @@
 package metapenta.commands;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+
 import metapenta.model.MetabolicNetwork;
 import metapenta.model.MetabolicNetworkXMLLoader;
 
@@ -7,15 +12,19 @@ public class ConnectedComponents {
 	/**
 	 * The main method of class
 	 * @param args[0] the path of the XML file 
-	 * @param args[1] The initial metabolites separated by comma
-	 * @param args[2] Target metabolite
-	 * @param args[3] fileName1 Metabolic Network In CSV	
+	 * @param args[1] out file in csv	
 	 * @throws Exception if exists any error of I/O
 	 */
 	public static void main(String[] args) throws Exception {
 		MetabolicNetworkXMLLoader loader = new MetabolicNetworkXMLLoader();
 		MetabolicNetwork network = loader.loadNetwork(args[0]);
 		network.makeNet();
-		network.connectedComponents();		
+		Map<String, Integer> connectedComponents = network.connectedComponents();
+		StringBuilder csv = new StringBuilder();
+		Set<String> reactionsSet = connectedComponents.keySet();
+		for (String reaction : reactionsSet) {
+			csv.append(reaction+","+connectedComponents.get(reaction)+"\n");
+		}
+		Files.write(Paths.get(args[1]), csv.toString().getBytes());
 	}
 }
